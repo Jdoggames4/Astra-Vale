8ddbd36606e1a667b5d63ed6d1591ee77c5873b5
Jdoggames4 <jdoggames4@gmail.com>
2026-05-13 12:58:12 -0600

    Update Platformer game.py

--- Platformer game.py
+++ Platformer game.py
@@ -471,7 +471,7 @@ def get_block(size):
 class Player(pygame.sprite.Sprite):
       COLOR = (255,0,0)
       GRAVITY = 1
-      SPRITES = load_sprite_sheets("MainCharacters" , "PinkMan" ,32,32,True)
+      SPRITES = load_sprite_sheets("Character" , "PinkMan" ,32,32,True)
       ANIMATION_DELAY = 8
 
 
